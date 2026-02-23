5be79050f6974f0e519a8cc835603a4c28f52557
Bhushan <bhushanlahore03@gmail.com>
2026-02-23 10:29:13 +0530

    Immutable class demo

---
+++ src/com/corejavaconcepts/ImmuatableClass.java
@@ -0,0 +1,15 @@
+package com.corejavaconcepts;
+
+public final class ImmuatableClass {
+
+    private final int id;
+    private final String name;
+
+    public ImmuatableClass(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+
+    //getter methods
+}

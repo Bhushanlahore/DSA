0674210692e81d19cc349075c707da8e8244f9fb
Bhushan <bhushanlahore03@gmail.com>
2026-02-23 09:10:07 +0530

    Best time to buy and sell stock

---
+++ src/com/practice/javacode/BestTimeToBuySellStock.java
@@ -0,0 +1,27 @@
+package com.practice.javacode;
+
+public class BestTimeToBuySellStock {
+    public static int maxProfit(int[] prices) {
+
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for(int price: prices){
+            if(price < minPrice){
+                minPrice = price;  //updating min price
+            }else{
+                int profit = price - minPrice;
+                maxProfit = Math.max(profit, maxProfit);
+            }
+
+
+        }
+
+        return maxProfit;
+    }
+
+    public static void main(String[] args) {
+        int[] prices = {1, 7, 5, 3, 6,1,5,9,10,2, 4};
+        System.out.println(maxProfit(prices)); // Output: 5
+    }
+}

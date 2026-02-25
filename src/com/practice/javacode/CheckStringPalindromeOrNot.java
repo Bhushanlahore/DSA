1fc4aad67ef5f40372a0cd84714ff42a59dd59ed
Bhushan <bhushanlahore03@gmail.com>
2026-02-25 12:34:55 +0530

    check palindrome or not ignoring special characters

--- src/com/practice/javacode/CheckStringPalindromeOrNot.java
+++ src/com/practice/javacode/CheckStringPalindromeOrNot.java
@@ -17,11 +17,37 @@ public class CheckStringPalindromeOrNot {
         return true;
     }
 
+    public static boolean isPalindromeIgnoreSpacialCharacter(String s){
+
+        //"A man, a plan, a canal: Panama"
+        if (s == null) return false;
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+            // Skip non-alphanumeric
+            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
+                left++;
+            }
+            while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
+                right--;
+            }
+            if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
 
     public static void main(String args[]){
-
         String str = "nacnbcan";
-
         System.out.println(checkPalindrome(str));
+
+        String s = "A man, a plan, a canal: Panama";
+
+        System.out.println(isPalindromeIgnoreSpacialCharacter(s));
+
     }
 }

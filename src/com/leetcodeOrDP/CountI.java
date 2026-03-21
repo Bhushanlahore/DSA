4190d75edbb60914db048394eb6e6a3ea785220d
Bhushan <bhushanlahore03@gmail.com>
2026-03-22 00:39:56 +0530

    Update CountI class to calculate maximum sum of a specific pattern in a matrix

--- src/com/leetcodeOrDP/CountI.java
+++ src/com/leetcodeOrDP/CountI.java
@@ -4,10 +4,10 @@ public class CountI {
         public static void main(String[] args) {
 
             int[][] mat = {
-                    {1,1,1,1},
-                    {1,1,1,1},
-                    {1,1,1,1},
-                    {1,1,1,0}
+                    {1,11,17,11},
+                    {1,5,1,11},
+                    {3,14,61,1},
+                    {12,31,37,10}
             };
 
             int count = countIPatterns(mat);
@@ -19,20 +19,25 @@ public class CountI {
 
             int m = mat[0].length;
             int count = 0;
+            int maxSum = 0;
 
             for (int i = 0; i <= n - 3; i++) {
                 for (int j = 0; j <= m - 3; j++) {
 
-                    if (mat[i][j] == 1 && mat[i][j+1] == 1 && mat[i][j+2] == 1 &&
-                            mat[i+1][j+1] == 1 &&
-                            mat[i+2][j] == 1 && mat[i+2][j+1] == 1 && mat[i+2][j+2] == 1) {
+                    int sum = mat[i][j] +  mat[i][j+1] + mat[i][j+2] +mat[i+1][j+1]+mat[i+2][j]+mat[i+2][j+1]+mat[i+2][j+2];
 
-                        count++;
-                    }
+                    maxSum = Math.max(maxSum, sum);
+
+//                    if (mat[i][j] == 1 && mat[i][j+1] == 1 && mat[i][j+2] == 1 &&
+//                            mat[i+1][j+1] == 1 &&
+//                            mat[i+2][j] == 1 && mat[i+2][j+1] == 1 && mat[i+2][j+2] == 1) {
+//
+//                        count++;
+//                    }
                 }
             }
 
-            return count;
+            return maxSum;
         }
 
 }
